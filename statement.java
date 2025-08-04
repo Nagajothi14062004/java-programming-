18f5a8aac999cf5f2cc57ae9a0673cd86eeaaf96
Nagajothi14062004 <nagajothi9379@mountzion.ac.in>
2025-08-03 23:56:36 -0700

    Create statement.java

---
+++ statement.java
@@ -0,0 +1,12 @@
+class  main{
+    public static void main(String[] args) {
+     int x=  18;
+     int y=  21;
+ if(y>x){
+        
+        System.out.println("major");
+    }
+else{
+  System.out.println("minor");
+}}
+}
